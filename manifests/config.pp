43f21089a7d64011f2305fb29c3c3f9f9ef8b33f
rags ubu lap <rags@me.com>
2019-06-08 23:38:50 +0530

    added config.pp

---
+++ manifests/config.pp
@@ -0,0 +1,44 @@
+class nginx::config (
+  String $ensure                = $::nginx::config_ensure,
+  String $config_dir            = $::nginx::config_dir,
+  String $mode                  = $::nginx::config_dir,
+  String $owner                 = $::nginx::config_owner,
+  String $group                 = $::nginx::config_group,
+  String $confd                 = $::nginx::config_confd,
+  String $log_dir               = $::nginx::config_log_dir,
+  String $pid_file              = $::nginx::config_pid_file,
+  Optional[String] $vdir_enable = $::nginx::config_vdir_enable,
+  String $user                  = $::nginx::config_process_user,
+  String $docroot               = $::nginx::docroot,
+
+  ){
+    file {'nginx.conf':
+
+      ensure        => $ensure,
+      path          => "${config_dir}/nginx.conf",
+      mode          => $mode,
+      owner         => $owner,
+      user          => $user,
+      group         => $group,
+      content       => template("${module_name}/conf.d/nginx.conf.erb")
+
+   }
+
+   file {"$logdir":
+
+      ensure        => directory,
+      recurse       => true,
+
+    }
+
+    file {"$docroot":
+
+       ensure       => directory,
+       recurse      => true,
+       owner        => $owner,
+       group        => $group,
+       mode         => '775',
+
+     }
+
+  }
