21c43a85750f7f7b3d910252b5ab185d0b9e89f1
rags <rags@red.com>
2019-06-09 13:49:54 +0000

    modded params and service

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,12 +1,12 @@
 class nginx::service (
-  String ensure          = $::nginx::service_ensure,
-  String service_name    = $::nginx::service_name,
-  String pattern         = $::nginx::service_pattern,
-  Boolean enable         = $::nginx::service_enable,
-  Boolean hasstatus      = $::nginx::service_hasstatus,
-  Boolean hasrestart     = $::nginx::service_hasrestart,
+  String $ensure          = $::nginx::service_ensure,
+  String $service_name    = $::nginx::service_name,
+  String $pattern         = $::nginx::service_pattern,
+  Boolean $enable         = $::nginx::service_enable,
+  Boolean $hasstatus      = $::nginx::service_hasstatus,
+  Boolean $hasrestart     = $::nginx::service_hasrestart,
   ){
-    service {'service_name':
+    service {'nginx_service':
 
      ensure      => $ensure,
      name        => $service_name,
